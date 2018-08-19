b0fdf5882ecc27092579048d18e8079a393e907b
Mitsuya.Watanabe <watanabe@gocco.co.jp>
2018-08-19 12:43:13 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <hello-world />
+  </div>
+</template>
+
+<script>
+import NativeCommunicator from '@/js/NativeCommunicator.js'
+import HelloWorld from '@/components/HelloWorld'
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'App',
+  mounted() {
+    console.log('mounted');
+  },
+  components: {
+    HelloWorld
+  },
+  methods: {}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <img src="../assets/logo.png">
+    <!-- <router-view/> -->
+    <p>message from native:{{getMessageFromNative}}</p>
+    <p>message to native:{{message}}</p>
+  </div>
+</template>
+
+<script>
+import NativeCommunicator from '@/js/NativeCommunicator.js'
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'HelloWorld',
+  computed: {
+    ...mapGetters([
+      'getMessageFromNative'
+    ])
+  },
+  data() {
+    return {
+      message:""
+    }
+  },
+  watch: {
+    getMessageFromNative: function (oldVal, newVal) {
+      if (oldVal != newVal) {
+        this.message = new Date().toString()
+        NativeCommunicator.helloNative(this.message)
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
